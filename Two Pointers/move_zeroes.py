37060c1487133c6266adc1817bca78a7e591d5b9
Fazliddin Mamutkhanov <103223186+fazliddinorg@users.noreply.github.com>
2025-10-01 16:15:20 +0900

    move_zeroes.py

--- Two Pointers/move_zeroes.py
+++ Two Pointers/move_zeroes.py
@@ -9,18 +9,3 @@ class Solution1:
             if right != 0:
                 nums[left], nums[i] = nums[i], nums[left]
                 left += 1
-
-
-
-
-# SOLUTION 2: Using Extra Space
-# Time: O(n), Space: O(n)
-class Solution2:
-    def moveZeroes(self, nums: List[int]) -> None:
-        non_zeros = [num for num in nums if num != 0]
-        num_zeros = len(nums) - len(non_zeros)
-        
-        for i in range(len(non_zeros)):
-            nums[i] = non_zeros[i]
-        for i in range(len(non_zeros), len(nums)):
-            nums[i] = 0
